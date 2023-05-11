33337ee67aea2c09eda11aa560b1cdf3cb8b48a5
Bella <bellasuto2@gmail.com>
2023-05-11 10:01:16 -0300

    adicionando log

--- mscartoes/src/main/java/io/microsservice/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/io/microsservice/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -8,12 +8,14 @@ import io.microsservice.mscartoes.domain.DadosSolicitacaoEmisssaoCartoes;
 import io.microsservice.mscartoes.infra.repository.CartaoRepository;
 import io.microsservice.mscartoes.infra.repository.ClienteCartaoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EmissaoCartaoSubscriber {
     private final CartaoRepository cartaoRepository;
     private final ClienteCartaoRepository clienteCartaoRepository;
@@ -32,7 +34,7 @@ public class EmissaoCartaoSubscriber {
             clienteCartaoRepository.save(clienteCartao);
 
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            log.error("Erro ao receber solicitacao de emissao de cartao: {}", e.getMessage());
         }
     }
 
